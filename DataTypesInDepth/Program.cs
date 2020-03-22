6abf718d61ce4b73c43e049ddb0b0720090a09d7
Nepcam <cameron.nepe@gmail.com>
2020-03-22 21:40:36 +1300

    typeof().IsPrimitive

--- DataTypesInDepth/Program.cs
+++ DataTypesInDepth/Program.cs
@@ -12,13 +12,20 @@ namespace DataTypesInDepth
         {
             int x = 0;
 
-            x = int.MaxValue;
+            x = int.MinValue;
 
             Console.WriteLine(x);
 
-            uint y = uint.MaxValue;
+            uint y = uint.MinValue;
 
             Console.WriteLine(y);
+
+            Console.WriteLine(typeof(int).IsPrimitive);
+            Console.WriteLine(typeof(float).IsPrimitive);
+            Console.WriteLine(typeof(double).IsPrimitive);
+            Console.WriteLine(typeof(decimal).IsPrimitive);
+            Console.WriteLine(typeof(char).IsPrimitive);
+            Console.WriteLine(typeof(string).IsPrimitive);
         }
     }
 }
